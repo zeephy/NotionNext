ef1f01e4327fc3f731c1ae52aad89444d3db2fb4
zeephy <86136507+zeephy@users.noreply.github.com>
2023-10-23 22:43:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,15 +17,15 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '空格Zephyr', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '我是空格', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '产品经理丨创作者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://pmplanet.club', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '产品经理, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '产品经理, 个人成长，效率工具，空周刊', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'wzfh520@gmail.com', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
-  CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
+  CONTACT_JIKE: process.env.NEXT_PUBLIC_CONTACT_JIKE || 'https://web.okjike.com/u/695ACB1F-CBA8-4896-B105-4FE7981E4300', // 你的微博个人主页
+  CONTACT_WECHAT: process.env.NEXT_PUBLIC_CONTACT_WECHAT || 'https://mp.weixin.qq.com/s/OG9wuDDOZBJWaB06mMOa0A', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
@@ -33,7 +33,7 @@ const BLOG = {
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
 
-  NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
+  NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://pmplanet.notion.site/fe9a6051d6cc4d0d957cfc7b631a53f0?pvs=4', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
 
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
 

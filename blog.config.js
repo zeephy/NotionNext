cf7d8937eb7f5a3db80d8506e5662d20aade9d85
zeephy <86136507+zeephy@users.noreply.github.com>
2023-10-23 22:32:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,10 +17,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '空格Zephyr', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '产品经理丨创作者', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://pmplanet.club', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '产品经理, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'wzfh520@gmail.com', // 邮箱地址 例如mail@tangly1024.com
